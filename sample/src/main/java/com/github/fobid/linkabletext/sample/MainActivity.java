38a4b9ee6992988d05eb57cc676d12799c3ef415
sungtae.lim <sungtae.lim@partner.so>
2016-11-17 11:50:46 +0900

    first commit

---
+++ sample/src/main/java/com/github/fobid/linkabletext/sample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.github.fobid.linkabletext.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main);
+    }
+}
